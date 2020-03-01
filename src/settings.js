dc6f268ddae54723a14efac3853d76e18914ada3
yichanglengyu <384387481@qq.com>
2020-03-01 15:12:41 +0800

    inti

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '复习提醒助手控制台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
